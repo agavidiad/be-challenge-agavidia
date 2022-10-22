bde8f830bc105422c2137a75adf6fca84fb571e5
Alex Gavidia <alex@eucim.es>
2022-10-22 11:28:18 -0500

    delete Teams

--- Backend.Node.Football/src/components/teams/network.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./controller');
-
-
-router.get("/:name/:players?", controller.getTeams)
-
-module.exports = router;
